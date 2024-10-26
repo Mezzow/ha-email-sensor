f932ec04b95c72c955707e374c3f097ec0b25f17
Mezzow <13226932+Mezzow@users.noreply.github.com>
2024-10-27 00:13:22 +0200

    amazon_de: removed debug logging, some cleanup

--- custom_components/email/parsers/amazon_de.py
+++ custom_components/email/parsers/amazon_de.py
@@ -9,31 +9,37 @@ _LOGGER = logging.getLogger(__name__)
 ATTR_AMAZON_DE = 'amazon_de'
 EMAIL_DOMAIN_AMAZON_DE = 'amazon.de'
 
+# compile regex patterns
+BODY_ORDER_PATTERNS = re.compile(r'(?:Order|Bestellnummer): #(.*?)\n')
+SUBJECT_ORDER_PATTERNS = re.compile(
+    r'Your Amazon\.de order of (.*?) has been dispatched!|'
+    r'Deine Amazon\.de-Bestellung mit (.*?) wurde versandt!'
+)
+TRACK_PACKAGE_PATTERNS = re.compile(
+    r'track your package|lieferung verfolgen',
+    re.IGNORECASE
+)
+
 def parse_amazon_de(email):
-    _LOGGER.debug(email)
     """Parse Amazon tracking numbers."""
     tracking_numbers = []
  
-    soup = BeautifulSoup(email[EMAIL_ATTR_BODY], 'html.parser')
 
     # see if it's an shipped order email
-    order_number_match = re.search('Order: #(.*?)\n', email[EMAIL_ATTR_BODY]) or re.search('Bestellnummer: #(.*?)\n', email[EMAIL_ATTR_BODY])
-    _LOGGER.debug(order_number_match)
+    order_number_match = BODY_ORDER_PATTERNS.search(email[EMAIL_ATTR_BODY])
     if not order_number_match:
-        order_number_match = re.search('Your Amazon.de order of (.*?) has been dispatched!', email[EMAIL_ATTR_SUBJECT]) or re.search('Deine Amazon.de-Bestellung mit (.*?) wurde versandt!', email[EMAIL_ATTR_SUBJECT])
-        _LOGGER.debug(order_number_match)
+        order_number_match = SUBJECT_ORDER_PATTERNS.search(email[EMAIL_ATTR_SUBJECT])
     if not order_number_match:
         return tracking_numbers
 
     order_number = order_number_match.group(1)
+    soup = BeautifulSoup(email[EMAIL_ATTR_BODY], 'html.parser')
 
     # find the link that has 'track your package' text
     link_elements = soup.find_all('a')
-    _LOGGER.debug(link_elements)
     for link_element in link_elements:
-        if not re.search(r'track your package', link_element.text, re.IGNORECASE) and not re.search(r'lieferung verfolgen', link_element.text, re.IGNORECASE):
+        if not TRACK_PACKAGE_PATTERNS.search(link_element.text):
             continue
-        _LOGGER.debug(link_element)
         # if found we no get url and check for duplicates
         link = link_element.get('href')
 
